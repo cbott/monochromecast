4ee760e27030aa4beed2f7721ab36caafed9224f
cbott <cbott6@gmail.com>
2018-12-09 06:39:08 +0000

    Initial commit with hardware stuff

---
+++ hardware.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as gpio
+
+PS_ON_PIN = 11  # GPIO 17
+DIM_PIN = 12  # GPIO 18 (PWM)
+
+PWM_FREQ = 100  # Hz
+
+class TV_Controller:
+    def __init__(self):
+        gpio.setmode(gpio.BOARD)
+        gpio.setup(PS_ON_PIN, gpio.OUT)
+        gpio.setup(DIM_PIN, gpio.OUT)
+        self.pwm = gpio.PWM(DIM_PIN, PWM_FREQ)
+        self.pwm.start(50)
+
+    def __del__(self):
+        self.disable_system()
+        gpio.cleanup()
+
+    def enable_system(self):
+        gpio.output(PS_ON_PIN, gpio.HIGH)
+
+    def disable_system(self):
+        gpio.output(PS_ON_PIN, gpio.LOW)
+
+    def set_brightness(self, brightness):
+        # Set the brightness 0 to 100%
+        brightness = max(0, min(brightness, 100))
+        # print("Setting brightness to", brightness)
+	# 100 = off, 0 = full on
+        self.pwm.ChangeDutyCycle(100 - brightness)
+
+# TODO: Cleanup
