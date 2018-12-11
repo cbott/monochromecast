66e39a36892ac8360d7c0991f5bc8c05d0a0d352
lethosor <lethosor@gmail.com>
2018-12-11 18:27:01 -0500

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+cd "$(dirname "$0")"
+if [ -z "$MONOCHROMECAST_HOST" ]; then
+    echo "MONOCHROMECAST_HOST not set"
+    exit 1
+fi
+rsync -avz . "$MONOCHROMECAST_HOST":MonochromeCast/ --exclude .git --exclude-from .gitignore
+ssh "$MONOCHROMECAST_HOST" 'pkill -HUP gunicorn'
