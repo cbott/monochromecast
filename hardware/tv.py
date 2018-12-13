60865cbeffc877b272dc837b4c38ac3a9f2e1616
cbott <cbott6@gmail.com>
2018-12-13 01:51:36 +0000

    Add method to change frequency

--- hardware/tv.py
+++ hardware/tv.py
@@ -3,15 +3,16 @@ import RPi.GPIO as gpio
 PS_ON_PIN = 11  # GPIO 17
 DIM_PIN = 12  # GPIO 18 (PWM)
 
-PWM_FREQ = 100  # Hz
+STARTUP_FREQ = 100  # Hz
+STARTUP_DC = 99  # % Duty Cycle
 
 class TV_Controller:
     def __init__(self):
         gpio.setmode(gpio.BOARD)
         gpio.setup(PS_ON_PIN, gpio.OUT)
         gpio.setup(DIM_PIN, gpio.OUT)
-        self.pwm = gpio.PWM(DIM_PIN, PWM_FREQ)
-        self.pwm.start(50)
+        self.pwm = gpio.PWM(DIM_PIN, STARTUP_FREQ)
+        self.pwm.start(STARTUP_DC)
         self.set_brightness(1)
         self.enable_system()
 
@@ -26,6 +27,11 @@ class TV_Controller:
     def disable_system(self):
         gpio.output(PS_ON_PIN, gpio.LOW)
         self.enabled = False
+
+    def set_frequency(self, freq):
+        """Set the PWM frequency in Hz"""
+        freq = max(0, freq)  # Cannot have negative frequency
+        self.pwm.ChangeFrequency(freq)
 
     def set_brightness(self, brightness):
         """Set the brightness (0 to 100%)"""
