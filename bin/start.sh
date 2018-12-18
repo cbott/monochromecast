4866323a2e0145e18c5b7be3cc6a983fc81a777d
lethosor <lethosor@gmail.com>
2018-12-18 15:12:25 -0500

    Run gunicorn with authbind

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd "$(dirname "$0")"/..
 ./hardware/start-hw-server
-tmux new-session -d -s gunicorn "gunicorn3 -b 0.0.0.0:8000 web:app"
+tmux new-session -d -s gunicorn "authbind gunicorn3 -b 0.0.0.0:80 web:app"
